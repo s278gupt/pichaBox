34db794dcbf8a6653df8a69b655dbb2dd958066a
s278gupt <s278gupt@edu.uwaterloo.ca>
2019-10-14 12:44:42 -0400

    more img validation

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,14 +4,23 @@ class Album < ApplicationRecord
     has_one_attached :header_image
     has_many_attached :images
 
-    validate :image_presence
-
-    def image_presence
-        errors.add(:images, "please select images") unless (images.attached? && header_image.attached?)
-    end
+    validates :description, presence: true
+    validates :header_image, presence: true
+    validate :image_type
 
     def image_resize index
         image = ActiveStorage::Attachment.find(index)
         return image.variant(resize: '500x500').processed
+    end
+
+    private
+    def image_type
+        errors.add(:images, "please select images") unless images.attached?
+
+        images.each do |img|
+            if !img.content_type.in?(%('image/jpeg' 'image/png'))
+                errors.add(:images, 'needs to be a JPEG or PNG')
+            end
+        end
     end
 end
